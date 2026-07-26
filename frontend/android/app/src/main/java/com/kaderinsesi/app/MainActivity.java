d32d7b8c67dba1bb31f1b18de0d37d17947f9478
Verdent <noreply@verdent.ai>
2026-07-26 01:29:09 +0000

    fix: GoogleAuth plugin package adi

--- frontend/android/app/src/main/java/com/kaderinsesi/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/kaderinsesi/app/MainActivity.java
@@ -2,7 +2,7 @@ package com.kaderinsesi.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitorgoogleauth.GoogleAuth;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
   @Override
